905b1dda4c3e3a38e7693107996fd864d30370e6
greinerchan <greinerchan@gmail.com>
2020-06-09 23:33:39 -0400

    Java programming review

---
+++ ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -0,0 +1,32 @@
+package academy.learnprogramming;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int integerA = 10_000;
+	    int intMin = Integer.MIN_VALUE;
+	    int intMax = Integer.MAX_VALUE;
+        System.out.println("IntMin" + intMin + intMax);
+
+        byte byteA = Byte.MIN_VALUE;
+        byte byteB = Byte.MAX_VALUE;
+        System.out.println("byte bits" + byteA + byteB);
+
+        short shortA = Short.MIN_VALUE;
+        short shortB = Short.MAX_VALUE;
+        System.out.println("short bits" + shortA + shortB);
+
+        long longVal = 1000L;
+        long longMin = Long.MIN_VALUE;
+        long longMax = Long.MAX_VALUE;
+        System.out.println("long value" + longMax + longMin);
+
+        float floatMin = Float.MIN_VALUE;
+        float floatMax = Float.MAX_VALUE;
+        System.out.println("float:" + floatMin + floatMax);
+
+        char charA = 'A';
+    }
+}
